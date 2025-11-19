1a67f7778ba005ca1d912dd8cee40e3101ce2210
shivamjaiswalsas <shivamj85@gmail.com>
2025-11-20 00:14:53 +0530

    button green

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ export default function App() {
             </button>
             <button
               onClick={() => setActiveTask(2)}
-              className="px-5 py-3 bg-blue-600 hover:bg-pink-700 text-white rounded-lg shadow"
+              className="px-5 py-3 bg-emerald-900 hover:bg-pink-700 text-white rounded-lg shadow"
             >
               Task 2
             </button>
